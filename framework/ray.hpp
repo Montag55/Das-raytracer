efb0261b9b69ffa2aaa4bce45e9cdc9a33735226
Looo911 <lo.gunreben@gmail.com>
2016-08-06 16:20:31 +0200

    Ray inversed für bessere Box intersect hinzugefügt

--- framework/ray.hpp
+++ framework/ray.hpp
@@ -12,12 +12,14 @@ struct Ray
 
 	Ray(glm::vec3 const& orig, glm::vec3 const& dir):
 		origin{orig},
-		direction{dir}
+		direction{dir},
+		inv_direction{glm::vec3{1.0/dir.x, 1.0/dir.y, 1.0/dir.z}}
 		{}
 
 
 	glm::vec3 origin;
 	glm::vec3 direction;
+	glm::vec3 inv_direction;
 };
 
 #endif
